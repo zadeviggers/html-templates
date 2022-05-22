5afd92cba210417867cb57b0b300166b8af21582
Zade Viggers <zade.viggers@gmail.com>
2022-05-22 20:23:53 +1200

    add audio, video, and image icons
    Signed-off-by: Zade Viggers <zade.viggers@gmail.com>

--- directory-list-template-literal.ts
+++ directory-list-template-literal.ts
@@ -9,6 +9,19 @@ interface Item {
 	url: string;
 }
 
+const imageExtentions = ["jpg", "jpeg", "png", "gif", "bmp", "svg"];
+const videoExtentions = [
+	"mp4",
+	"avi",
+	"mkv",
+	"mov",
+	"wmv",
+	"flv",
+	"mpg",
+	"mpeg",
+];
+const audioExtentions = ["mp3", "wav", "ogg", "flac", "aac"];
+
 export const generateTemplate = (
 	breadcrumbs: Breadcrumb[],
 	items: Item[]
@@ -119,6 +132,41 @@ export const generateTemplate = (
 					<g id="folder-icon">
 						<path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
 					</g>
+					<g id="image-icon">
+						<rect
+							x="3"
+							y="3"
+							width="18"
+							height="18"
+							rx="2"
+							ry="2"
+						></rect>
+						<circle cx="8.5" cy="8.5" r="1.5"></circle>
+						<polyline points="21 15 16 10 5 21"></polyline>
+					</g>
+					<g id="video-icon">
+						<rect
+							x="2"
+							y="2"
+							width="20"
+							height="20"
+							rx="2.18"
+							ry="2.18"
+						></rect>
+						<line x1="7" y1="2" x2="7" y2="22"></line>
+						<line x1="17" y1="2" x2="17" y2="22"></line>
+						<line x1="2" y1="12" x2="22" y2="12"></line>
+						<line x1="2" y1="7" x2="7" y2="7"></line>
+						<line x1="2" y1="17" x2="7" y2="17"></line>
+						<line x1="17" y1="17" x2="22" y2="17"></line>
+						<line x1="17" y1="7" x2="22" y2="7"></line>
+					</g>
+					<g id="audio-icon">
+						<path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
+						<path
+							d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"
+						></path>
+					</g>
 				</defs>
 			</svg>
 		</div>
@@ -130,18 +178,32 @@ export const generateTemplate = (
 			)}
 		</nav>
 		<ul id="list">
-			${items.map(
-				(item) => `<li>
+			${items.map((item) => {
+				let iconID = item.folder ? "folder-icon" : "file-icon";
+				if (!item.folder) {
+					const split = item.name.split(".");
+					if (split.length > 1) {
+						const extension = split.pop();
+						if (imageExtentions.includes(extension))
+							iconID = "image-icon";
+						else if (videoExtentions.includes(extension))
+							iconID = "video-icon";
+						else if (audioExtentions.includes(extension))
+							iconID = "audio-icon";
+					}
+				}
+
+				return `<li>
 			<a href="${item.url}" title="${item.name}">
 				<svg class="icon" viewBox="0 0 24 24">
-					<use xlink:href="#${item.folder ? "folder" : "file"}-icon"></use>
+					<use xlink:href="#${iconID}"></use>
 				</svg>
 				<span class="text">
 					${item.name}
 				</span>
 			</a>
-		</li>`
-			)}
+		</li>`;
+			})}
 		</ul>
 	</body>
 </html>`;
